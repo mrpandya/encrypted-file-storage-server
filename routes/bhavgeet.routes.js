7b642500be8fd51b9a6ef28895a72c062d206889
manan <mrpandya2017@gmail.com>
2022-02-17 19:35:47 +0530

    scraped

--- routes/bhavgeet.routes.js
+++
@@ -1,9 +0,0 @@
-
-const controller = require("../controllers/bhavgeet.controller");
-
-module.exports = function (app) {
-
-  app.get('/bhavgeet/:bhavgeetCode', controller.getbhavgeet);
-
-  app.post('/bhavgeet', controller.savebhavgeet);
-};
